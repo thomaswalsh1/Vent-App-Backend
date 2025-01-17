7d230ca8bfef43b3c7519056d40b37a72a58d8cf
Thomas Walsh <130932404+thomaswalsh1@users.noreply.github.com>
2025-01-17 14:39:53 -0500

    CORS Middleware origin fix #1

--- server.js
+++ server.js
@@ -31,6 +31,12 @@ app.use(cors({
     credentials: true,
 }));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 
 app.use('/auth', authRoutes);
