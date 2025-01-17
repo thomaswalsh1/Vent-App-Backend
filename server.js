818a3a6c75491a661395c7c152301fbba049e4d6
Thomas Walsh <130932404+thomaswalsh1@users.noreply.github.com>
2025-01-17 14:44:18 -0500

    CORS Middleware origin fix #2

--- server.js
+++ server.js
@@ -1,50 +1,56 @@
-const express = require('express');
-const multer = require('multer');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const dotenv = require('dotenv');
+const express = require('express')
+const multer = require('multer')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const dotenv = require('dotenv')
 
 // routes
-const authRoutes = require('./routes/AuthRoutes.js');
-const postRoutes = require('./routes/PostRoutes.js');
-const userRoutes = require('./routes/UserRoutes.js');
-const reportRoutes = require('./routes/ReportRoutes.js');
+const authRoutes = require('./routes/AuthRoutes.js')
+const postRoutes = require('./routes/PostRoutes.js')
+const userRoutes = require('./routes/UserRoutes.js')
+const reportRoutes = require('./routes/ReportRoutes.js')
 
-const app = express();
+const app = express()
 
 // db
-mongoose.connect("mongodb://localhost:27017/social-app", {
+mongoose
+  .connect('mongodb://localhost:27017/social-app', {
     useNewUrlParser: true,
-	useUnifiedTopology: true,
-}).then(e => console.log("Connected to MongoDB")).catch(err => console.log(`Error: ${err}`))
+    useUnifiedTopology: true
+  })
+  .then(e => console.log('Connected to MongoDB'))
+  .catch(err => console.log(`Error: ${err}`))
 
 // configure environment
-dotenv.config();
+dotenv.config()
 
 // multer
 
-app.use(express.json({ limit: '100mb' }));
+app.use(express.json({ limit: '100mb' }))
 
-app.use(cors({
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  next()
+})
+
+app.use(
+  cors({
     origin: process.env.ORIGIN,
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-    credentials: true,
-}));
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    credentials: true
+  })
+)
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
-
-app.use('/auth', authRoutes);
-app.use('/posts', postRoutes);
-app.use('/users', userRoutes);
-app.use('/reports', reportRoutes);
-
+app.use('/auth', authRoutes)
+app.use('/posts', postRoutes)
+app.use('/users', userRoutes)
+app.use('/reports', reportRoutes)
 
 app.listen(3000, () => {
-    console.log("Now running on port 3000");
+  console.log('Now running on port 3000')
 })
